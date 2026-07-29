69b8098134dcc4df4fbec6a7b9ca0fad7ec5c7db
CaliTarheel <43831892+CaliTarheel@users.noreply.github.com>
2026-07-28 21:45:09 -0700

    Add rotation file editor

--- src/qt-widgets/ViewportWindow.h
+++ src/qt-widgets/ViewportWindow.h
@@ -92,6 +92,7 @@ namespace GPlatesViewOperations
 {
 	class CloneOperation;
 	class DeleteFeatureOperation;
+	class RotationFileEditorOperation;
 }
 
 namespace GPlatesQtWidgets
@@ -474,6 +475,9 @@ namespace GPlatesQtWidgets
 		pop_up_python_console();
 
 		void
+		handle_rotation_file_editor();
+
+		void
 		open_dataset_webpage();
 		
 	private:
@@ -513,6 +517,9 @@ namespace GPlatesQtWidgets
 
 		//! For deleting a feature.
 		boost::scoped_ptr<GPlatesViewOperations::DeleteFeatureOperation> d_delete_feature_operation_ptr;
+
+		//! For motion-preserving plate circuit edits in loaded rotation collections.
+		boost::scoped_ptr<GPlatesViewOperations::RotationFileEditorOperation> d_rotation_file_editor_operation_ptr;
 
 
 		/**
