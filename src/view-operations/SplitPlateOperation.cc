88fa92839b0809a31ab36155ebbb439fe38c49e5
CaliTarheel <43831892+CaliTarheel@users.noreply.github.com>
2026-07-31 13:32:56 -0700

    Harden split plate selection state
    (cherry picked from commit 63b4f07cc893134600814a1ea542432c38a2eb80)

--- src/view-operations/SplitPlateOperation.cc
+++ src/view-operations/SplitPlateOperation.cc
@@ -3,6 +3,19 @@
 /**
  * \file
  * Implements the World Building "Split Plate" operation.
+ *
+ * Copyright (C) 2026 CaliTarheel
+ *
+ * This file is part of GPlates.
+ *
+ * GPlates is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 2, as published by
+ * the Free Software Foundation.
+ *
+ * GPlates is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
  */
 
 #include <algorithm>
@@ -593,9 +606,18 @@ GPlatesViewOperations::SplitPlateOperation::trigger()
 	{
 		// Selecting another polygon while a split is pending is a convenient way
 		// to correct the first selection.
+		const GPlatesModel::FeatureHandle::iterator geometry_property =
+				(*reconstructed_feature_geometry)->property();
+		if (!geometry_property.is_still_valid())
+		{
+			return Result(
+					OPERATION_ERROR,
+					QObject::tr("The replacement polygon is not backed by an editable geometry property."));
+		}
+
 		d_captured_polygon = CapturedPolygon(
 				d_feature_focus.focused_feature(),
-				(*reconstructed_feature_geometry)->property(),
+				geometry_property,
 				(*reconstructed_feature_geometry)->get_non_null_pointer_to_const(),
 				focused_polygon->get_non_null_pointer(),
 				current_reconstruction_time);
