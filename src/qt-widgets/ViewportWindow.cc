154d75e0719fda5c3c70d8587c3b3be198761c07
CaliTarheel <43831892+CaliTarheel@users.noreply.github.com>
2026-07-31 13:39:38 -0700

    Clarify feature statistics input handling
    (cherry picked from commit 4068e8885993ca05de1154c6008e4c2cd8fc8420)

--- src/qt-widgets/ViewportWindow.cc
+++ src/qt-widgets/ViewportWindow.cc
@@ -1154,6 +1154,8 @@ GPlatesQtWidgets::ViewportWindow::handle_globe_feature_context_menu(
 		Qt::MouseButton button,
 		Qt::KeyboardModifiers modifiers)
 {
+	Q_UNUSED(modifiers);
+
 	if (button != Qt::RightButton || !is_on_globe)
 	{
 		return;
@@ -1171,6 +1173,8 @@ GPlatesQtWidgets::ViewportWindow::handle_map_feature_context_menu(
 		Qt::MouseButton button,
 		Qt::KeyboardModifiers modifiers)
 {
+	Q_UNUSED(modifiers);
+
 	if (button != Qt::RightButton || !is_on_surface)
 	{
 		return;
