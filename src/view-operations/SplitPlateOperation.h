63b4f07cc893134600814a1ea542432c38a2eb80
CaliTarheel <43831892+CaliTarheel@users.noreply.github.com>
2026-07-31 13:32:56 -0700

    Harden split plate selection state

--- src/view-operations/SplitPlateOperation.h
+++ src/view-operations/SplitPlateOperation.h
@@ -2,7 +2,20 @@
 
 /**
  * \file
- * Implements the World Building "Split Plate" operation.
+ * Declares the World Building "Split Plate" operation.
+ *
+ * Copyright (C) 2026 CaliTarheel
+ *
+ * This file is part of GPlates.
+ *
+ * GPlates is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 2, as published by
+ * the Free Software Foundation.
+ *
+ * GPlates is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
  */
 
 #ifndef GPLATES_VIEWOPERATIONS_SPLITPLATEOPERATION_H
