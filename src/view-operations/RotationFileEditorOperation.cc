950c610b1954837fea6d9abe43ca45dd4f3acfba
CaliTarheel <43831892+CaliTarheel@users.noreply.github.com>
2026-07-31 14:10:12 -0700

    Ignore disabled poles in rotation edit bounds
    (cherry picked from commit e640f6482b0877918ee9f6a2ab0193ee4c49279c)

--- src/view-operations/RotationFileEditorOperation.cc
+++ src/view-operations/RotationFileEditorOperation.cc
@@ -208,7 +208,8 @@ namespace
 			for (GPlatesModel::RevisionedVector<GPlatesPropertyValues::GpmlTimeSample>::const_iterator sample_iter =
 					sampling->time_samples().begin(); sample_iter != sampling->time_samples().end(); ++sample_iter)
 			{
-				if (!sample_iter->valid_time()->get_time_position().is_real())
+				if (sample_iter->is_disabled() ||
+						!sample_iter->valid_time()->get_time_position().is_real())
 				{
 					continue;
 				}
@@ -299,7 +300,8 @@ namespace
 					sequence_iter->sampling->time_samples().begin();
 					sample_iter != sequence_iter->sampling->time_samples().end(); ++sample_iter)
 			{
-				if (sample_iter->valid_time()->get_time_position().is_real())
+				if (!sample_iter->is_disabled() &&
+						sample_iter->valid_time()->get_time_position().is_real())
 				{
 					const double time = sample_iter->valid_time()->get_time_position().value();
 					if (time > begin_time && time < end_time)
