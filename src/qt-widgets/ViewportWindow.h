52b9a57f083a479fb8c247c2046a6a753c7fab37
CaliTarheel <43831892+CaliTarheel@users.noreply.github.com>
2026-08-02 18:41:57 -0700

    Add Project Documents dock
    (cherry picked from commit 0c89a255ea5f6bf82f5d948c126be7b4d427ef9b)

--- src/qt-widgets/ViewportWindow.h
+++ src/qt-widgets/ViewportWindow.h
@@ -118,6 +118,7 @@ namespace GPlatesQtWidgets
 	class PythonConsoleDialog;
 	class ReconstructionViewWidget;
 	class SearchResultsDockWidget;
+	class ProjectDocumentsDockWidget;
 	class SmallCircleManager;
 	class TaskPanel;
 
@@ -186,6 +187,9 @@ namespace GPlatesQtWidgets
 
 		SearchResultsDockWidget &
 		search_results_dock_widget();
+
+		ProjectDocumentsDockWidget &
+		project_documents_dock_widget();
 
 		GlobeCanvas &
 		globe_canvas();
@@ -656,6 +660,9 @@ namespace GPlatesQtWidgets
 		 * A tabbed toolbar for the canvas tools.
 		 */
 		QPointer<CanvasToolBarDockWidget> d_canvas_tools_dock_ptr;
+
+		//! Associated Markdown documents, editor/preview and project metadata status.
+		QPointer<ProjectDocumentsDockWidget> d_project_documents_dock_ptr;
 
 		/**
 		 * The central widget in the main window containing everything except the menubar,
